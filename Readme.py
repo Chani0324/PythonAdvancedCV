5009fb2f0a0e6fa706de9daafef493ddc9014cd1
HongChan Yoon <hongchani0324@gmail.com>
2022-03-11 03:59:32 +0900

    PythonAdvancedCVWork Day04

---
+++ Readme.py
@@ -0,0 +1,2 @@
+# hand_tracking_module.py is main
+# the others are not necessary. just for test
--- my_hand_tracking.py
+++ my_hand_tracking.py
@@ -24,3 +24,5 @@ while True:
     cv2.putText(img, str(round(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
+
+    
